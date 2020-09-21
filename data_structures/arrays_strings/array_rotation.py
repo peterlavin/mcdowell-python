ec4b5b12500a11818fd175f5b2e1cb2f87f6a9c1
peterlavin <peterlavin01@gmail.com>
2020-09-21 11:06:08 +0100

    Comments improved

--- data_structures/arrays_strings/array_rotation.py
+++ data_structures/arrays_strings/array_rotation.py
@@ -8,42 +8,41 @@ from pprint import pprint as pp
 
 
 def rotate_array(image: []) -> []:
+    """ Rotates an array in place, assumes the array will be square"""
     
     # depth here means how many layers in from the outside
     # until the middle is reached. If n is odd, the middle
-    # value is not moved
-    print('\ndepth ranges from 0 to', int(len(image) / 2))
-    
-    # depth always starts at 0 and then up to half the length
+    # value is not moved. Depth will range from 0 to half
+    # of the length or an array side
     for depth in range(0, int(len(image) / 2)):
 
-        # For the outer layer, i.e. depth=0, last is
-        # the hightest index in a given row, it decreases
-        # as depth increases
-        last = len(image) - 1 - depth
+        # last is the hightest index in a given row,
+        # it decreases as depth increases
+        last = (len(image) - 1) - depth
         
         for i in range(depth, last):
             # When moving around at a given depth, offset is how
             # far around we have come. It steps back 1 for each
             # lap at a given depth
-            # Starting off, offset=0, or 0-0.
+            # Starting off at each depth, offset=0,
             # When offset is 0, the four corners of the array are shifted.
             # When offset increments, it is subtracted from the [x][] or [][y]
             # value as appropriate.
             offset = i - depth
-            # save first to temp variable
+            
+            # save top left to temp variable
             temp = image[depth][i]
             
-            # left > first location
+            # to right moves to top left  (0,0) < (n,0)
             image[depth][i] = image[last - offset][depth]
             
-            # btm to left
+            # btm right to top right (n,0) < (n,n)
             image[last - offset][depth] = image[last][last - offset]
             
-            # right > btm
+            # btm left to btm right (n,n) < (0,n)
             image[last][last - offset] = image[i][last]
             
-            # top > right
+            # temp < (0,n)
             image[i][last] = temp
     
     return image
@@ -58,7 +57,7 @@ if __name__ == '__main__':
    
     image = []
     count = 0
-    n = 4  
+    n = 5 
     for j in range(0, n):
         image.append([])
         for k in range(0, n):
