bd88ee0df3ebae6fe6f1a3f9467c4c50f0a7eeb9
peterlavin <peterlavin01@gmail.com>
2020-10-15 20:56:04 +0100

    One insert away working, WIP

--- data_structures/arrays_strings/1_5_one_away.py
+++ data_structures/arrays_strings/1_5_one_away.py
@@ -4,7 +4,6 @@ Created on Oct 2020
 @author: peter
 '''
 
-
 def one_edit_away(str_one: str, str_two: str) -> bool:
     
     if len(str_one) == len(str_two):
@@ -33,12 +32,30 @@ def one_replace_away(str_one,str_two):
 
 
 def one_insert_away(str_one,str_two):
+    ''' Called when one string is 1 char longer than the other,
+    therefore they can be made the same with one 'insert' '''
+    
+    index_one = 0
+    index_two = 0
+    
+    ''' Start along the string, check chars are the same, if a char is
+    different, this is ok ONCE, jump ahead one char in the longer string
+    and continue checking. If a second diff is found, return False '''
+    
+    while(index_one < len(str_one) and index_two < len(str_two)):
+        if str_one[index_one] != str_two[index_two]:
+            if index_one != index_two:
+                return False
+            index_two +=1
+        else:
+            index_one +=1
+            index_two +=1
+    
     return True
     
-    pass
 
 
 if __name__ == '__main__':
     
-    print(one_edit_away('oooy', 'only'))
+    print(one_edit_away('onlcy', 'only')) # this is wrong !!!
     pass
