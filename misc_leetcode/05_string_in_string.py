bf93f6c27b4cb5faa1548d5a529bb4b9051c604b
peterlavin <peterlavin01@gmail.com>
2020-10-31 10:59:07 +0000

    str in str finished

--- misc_leetcode/05_string_in_string.py
+++ misc_leetcode/05_string_in_string.py
@@ -6,12 +6,25 @@ Created on 30 Oct 2020
 
 
 def find_needle(long, short):
+    
+    if len(long) < len(short):
+        return -1
 
     for i in range(len(long) - len(short) + 1):
         if long[i:i + len(short)] == short:
             return i
 
+    return -1
 
-ans = find_needle('xyzabcuvw', 'vw')
-print('\nIndex where substring starts:', ans)
-        
+if __name__ == '__main__':
+    
+    long  = 'the quick brown fox jumped over the lazy dogs'
+    short = 'jump'
+    
+    ans = find_needle(long, short)
+    print('\nIndex where substring starts:', ans)
+    
+    if ans != -1:
+        print('-'*ans, short, '-'*(len(long)-len(short)-ans),'\n',long,  sep='')
+    else:
+        print('0'* len(long), '\n', long, sep='')
